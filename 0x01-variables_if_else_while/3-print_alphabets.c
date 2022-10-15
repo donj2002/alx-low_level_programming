259c3dcd3dcc5a0ac24707d874ac8eba2cc713fb
donj2002 <donaldnwajiaku@gmail.com>
2022-10-15 08:42:11 -0700

    c coding

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -15,13 +15,13 @@
 int main(void)
 {
 	char low;
-	
-	for(low = 'a'; low <= 'z'; low++)
+
+	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-	for(low = 'A'; low <= 'Z'; low++)
+	for (low = 'A'; low <= 'Z'; low++)
 		putchar(low);
-	
+
 	putchar('\n');
-	
-	return(0);
+
+	return (0);
 }
